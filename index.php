e787667dd761c77fe07228453ede6e421a0a2404
icaroluan <ilrf13@hotmail.com>
2019-06-10 23:15:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,20 +12,23 @@
 
         <!--<form class="form-horizontal" action="editar/confirmarLogin.php" method="post">-->
         <form class="form-horizontal" action="indexAlt.php" method="post">   
-        <div>
-            <input type="text" name="usuario" id="email" placeholder="Digite seu login"> 
-        </div>
-        
-        <div>
-            <input type="password" name="senha" id="senha" placeholder="Digite sua senha">
-        </div>
-        <div class="form-actions">
-            <button type="submit">Login</button>
-        </div>
-        <br>
-        <br>
-        <a href="../html/cadastroLogin.php" class="btn">Cadastre-se</a>
-</form>
+            <div>
+                <input type="text" name="usuario" id="email" placeholder="Digite seu login"> 
+            </div>
+            <div>
+                <input type="password" name="senha" id="senha" placeholder="Digite sua senha">
+            </div>
+            <div class="form-actions">
+                <button type="submit">Login</button>
+            </div>
+            <br>
+            <?php 
+                if(@$_REQUEST['success'] == 'false'){
+                    echo "Login e/ou senha incorreto(s)";
+                }
+                ?>
+            <a href="../html/cadastroLogin.php" class="btn">Cadastre-se</a>
+          </form>
         
     </div>
    
